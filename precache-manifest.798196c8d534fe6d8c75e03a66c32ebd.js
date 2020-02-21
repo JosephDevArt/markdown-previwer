f3277cef9bcfee3a0669b051d8b116de07430cb0
JosephDevArt <jbdevart@gmail.com>
2020-02-21 20:30:13 +0300

    Updates

---
+++ precache-manifest.798196c8d534fe6d8c75e03a66c32ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fbfe074af8a4d5cda773dc39beacd43",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9baf706e1161901a253b",
+    "url": "./static/css/main.29205866.chunk.css"
+  },
+  {
+    "revision": "a8d0c9709ea94753b0f1",
+    "url": "./static/js/2.154fcf96.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.154fcf96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9baf706e1161901a253b",
+    "url": "./static/js/main.a0b60afb.chunk.js"
+  },
+  {
+    "revision": "05eb702fb2dca6c34140",
+    "url": "./static/js/runtime-main.116aa83a.js"
+  }
+]);
